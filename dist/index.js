0ec050482932b0b3dd3a473952643a555a680cc1
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-07-21 22:51:05 +0200

    Update dependencies.

--- dist/index.js
+++ dist/index.js
@@ -77,7 +77,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	/**
 	 * This is a simple class used to manage dependencies between entities.
@@ -452,7 +452,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 3 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
 	module.exports = __WEBPACK_EXTERNAL_MODULE_3__;
 
